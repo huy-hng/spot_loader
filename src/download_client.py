47bf764268cd1b188194952370bf65478dc9a760
Huy <huy@hhoang.dev>
2021-11-13 20:36:58 +0100

    refactor retrier into helper function

--- src/download_client.py
+++ src/download_client.py
@@ -1,34 +1,31 @@
 import json
+from typing import Callable
 
 import requests
 from requests import Response
 
 from song import MP3JuicesSongType
 
-SEARCH_URL = 'https://myfreemp3juices.cc/api/search.php?callback=jQuery213021082371575984715_1635945826190'
 
 class MP3Juices:
+	SEARCH_URL = 'https://myfreemp3juices.cc/api/search.php?callback=jQuery213021082371575984715_1635945826190'
 
-	def get_songs(self, search_query: str):
+	def get_song_versions(self, search_query: str):
 		q = search_query.replace(' ', '+')
-		payload = {'q': q}
 
-		# retry until proper response or 20 tries
-		tries = 20
-		parsed = None
-		while parsed is None and tries > 0:
-			res = requests.post(SEARCH_URL, data=payload)
-			parsed = self.parse_response(res)
-			tries -= 1
+		try:
+			data = self.retrier(self.get_data, q)
+		except Exception as e:
+			print(f"Couldn't find {search_query} on MP3JUices")
 
-		if tries == 0:
-			raise Exception('No tries left')
-
-		songs: list[MP3JuicesSongType] = parsed[1:]
+		songs: list[MP3JuicesSongType] = data[1:]
 		return songs
 
 
-	def parse_response(self, res: Response):
+	def get_data(self, search_query):
+		data = {'q': search_query}
+		res = requests.post(self.SEARCH_URL, data=data)
+
 		text = res.text
 		start_index = text.find('{')
 		end_index = text.rfind('}')
@@ -36,3 +33,30 @@ class MP3Juices:
 		parsed = json.loads(text[start_index:end_index+1])
 		return parsed['response']
 
+
+	def download_song(self, song: MP3JuicesSongType):
+		try:
+			return self.retrier(self.request, song['url'])
+		except Exception as e:
+			print(f"Couldn't download {song['title']} by {song['artist']}")
+
+
+	def request(self, url: str):
+		res = requests.get(url, stream=True)
+		if res.status_code == 404:
+			return None
+		return res
+
+	
+
+	@staticmethod
+	def retrier(fn: Callable, *args, tries:int=20):
+		result = None
+		while result is None and tries > 0:
+			result = fn(args)
+			tries -= 1
+
+		if tries == 0:
+			raise Exception(f'No tries left for {fn.__name__} with args {args}')
+
+		return result
