0a33ba56e6acc1f19b21fd2c9b70842828ff7e07
Huy <huy@hhoang.dev>
2021-11-21 23:55:05 +0100

    parallelization and finally working

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # %%
+import os
+
+from concurrent.futures import ThreadPoolExecutor
+
 from src.spotify import Spotipy
 from src.download_client import MP3Juices
 from src.file_handler import FileHandler
@@ -11,19 +15,33 @@ fh = FileHandler('Tech House')
 tracks = sp.get_playlist_tracks(playlist_url)
 track_list = sp.tracks_to_list(tracks)
 
-for query in track_list:
-	print(f'Downloading {query}')
+
+def download_song(query):
+	print(f'\nDownloading {query}')
 
 	versions = mp3.get_song_versions(query)
 	if len(versions) == 0:
-		continue
+		print('No versions found. Skipping.')
+		return
 
 	version = versions[0]
+
+	name, location = fh.get_name_and_location(version)
+	if os.path.isfile(location):
+		# skip if file already exists
+		print('Song already downloaded.')
+		return
+
+
 	response = mp3.download_song(version)
 	if response is None:
-		break
-		continue
+		# break 
+		return
 
 	fh.write_song(version, response)
 	fh.edit_file_metadata(version)
-	break
+
+with ThreadPoolExecutor() as executor:
+	for query in track_list:
+		executor.submit(download_song, query)
+
