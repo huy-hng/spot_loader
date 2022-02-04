9c1fec28ae996714390288aed08d996aef690159
Huy <huy@hhoang.dev>
2022-02-05 00:00:30 +0100

    move everything to a main function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 from src.logger import log
 from src.downloader import Downloader
 
-if __name__ == '__main__':
+def main():
 	log.warning('\nStarting...')
 
 	DOWNLOADS_LOCATION = './downloads'
@@ -18,7 +18,15 @@ if __name__ == '__main__':
 	else:
 		url = input('Please input a URL.\n')
 
+	vpn = input('Are you using a VPN? [Y/N]\n')
+	if vpn.lower() != 'y':
+		return
+		
 	downloader = Downloader(DOWNLOADS_LOCATION, url)
 	downloader.download_playlists()
 
 	log.warning('Done.')
+
+
+if __name__ == '__main__':
+	main()
