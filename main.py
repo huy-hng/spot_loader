de04e9c5d689c788052473872e4c83c2a42ba063
Huy <huy@hoang.dev>
2022-11-02 15:33:55 +0100

    cleanup

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import time
 import os
 import json
-import concurrent.futures
 
 from src.logger import log
 from src import downloader
 from src import download_client
 from src import file_handler
-from src.tests import optimize_query, get_results
 
 def get_url():
 	settings_file = './settings.json'
@@ -21,6 +19,7 @@ def get_url():
 	vpn = input('Are you using a VPN? [Y/N]\n')
 	if vpn.lower() != 'y':
 		return
+	return url
 
 
 DOWNLOADS_LOCATION = './downloads'
@@ -33,39 +32,19 @@ def setup(url):
 	download_client.parse_url(url)
 
 
-def download():
+def main():
+	url = get_url()
+	if url is None: return
+
 	log.warning('\nStarting...')
 	start = time.perf_counter()
 
+	setup(url)
 	downloader.download_playlists()
 
 	end = time.perf_counter()
 	log.warning(f'Done. Time taken: {end-start}')
 
 
-def test():
-	url = 'https://myfreemp3juices.cc/'
-	playlist_url = 'https://open.spotify.com/playlist/0oWDXsY9BhT9NKimKwNY9d?si=bb79d60100714e1e'
-	start = time.perf_counter()
-
-	futures = optimize_query(url, playlist_url)
-	get_results(futures)
-
-	end = time.perf_counter()
-	print(f'1 time taken: {end-start}')
-
-
-def main():
-	# url = get_url()
-	url = 'https://myfreemp3juices.cc/'
-	if url is None:
-		return
-	setup(url)
-	download()
-
-
 if __name__ == '__main__':
-	# pass
 	main()
-	# test()
-
